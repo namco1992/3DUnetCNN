6a9a47d435bc6cd1daa871df22e05fb42550faba
namco <namco1992@gmail.com>
2017-10-01 21:22:16 +0800

    add config

---
+++ utils/config.py
@@ -0,0 +1,6 @@
+class MailSettings(object):
+    """Default Mail config"""
+    MAIL_HOST = "smtp.gmail.com:587"
+    MAIL_USER = "namco1992@gmail.com"
+    MAIL_PASS = ""
+    MAILTO_LIST = ["namco1992@gmail.com"]
